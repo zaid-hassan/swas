e4fdd08b8bc139652e19b27840bcb6c0696e5d2f
Zaid Hassan <zaidhassan251@gmail.com>
2026-03-18 16:05:03 +0530

    disable eslint

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     remotePatterns: [
       {
