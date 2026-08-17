f3a9884913089004da0093767ed78358999a13c2
Zaid Hassan <zaidhassan251@gmail.com>
2026-08-17 13:46:26 +0530

    successful build

--- types/products.ts
+++ types/products.ts
@@ -2,13 +2,13 @@ export type Product = {
   id: string;
   name: string;
   price: number;
-  image?: string;      // Cover image (used everywhere)
-  images?: string[];   // Gallery images (used on product page)
+  image: string;      // Cover image (used everywhere)
+  images: string[];   // Gallery images (used on product page)
   slug: string;
-  category?: string;
-  description?: string;
-  material?: string;
-  design?: string;
-  finish?: string;
-  idealFor?: string;
+  category: string;
+  description: string;
+  material: string;
+  design: string;
+  finish: string;
+  idealFor: string;
 };
