94eea5775f9ca5f2f20452883a5790fd733dc60f
Zaid Hassan <zaidhassan251@gmail.com>
2026-08-23 18:02:00 +0530

    fonts

--- app/(shell)/(public)/page.tsx
+++ app/(shell)/(public)/page.tsx
@@ -14,7 +14,7 @@ function page() {
       <CollectionFilm
         title="Taruni Collection"
         href="/collection/taruni"
-        video="https://res.cloudinary.com/dndppvnjl/video/upload/v1787232893/0820_vhpjoo.mp4"
+        video="https://res.cloudinary.com/dndppvnjl/video/upload/f_mp4,vc_h264,q_auto/v1787426419/577CAC23-E760-48F1-A08C-6615D47C1594_g1h6gl.mp4"
       />
       <Showcase />
       <Catalogue />
